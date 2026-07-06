1322f5a13a8820713cbe7fbb88eadf81680888f1
Dominic Spreitz <dominic@spreitz.de>
2026-07-06 20:23:18 +0200

    Revert "winch: migrate to the official lora driver (single-context radio)"
    This reverts commit 032fa12da1e0b3067661b063720e12912081055b.

--- firmware/fwbuild/manifest_winch.py
+++ firmware/fwbuild/manifest_winch.py
@@ -12,11 +12,8 @@ R = os.environ["WINCHY_REPO"]
 
 include("$(PORT_DIR)/boards/manifest.py")
 
-# Radio driver (official micropython-lib lora package, shared under rope/lib)
-# + OLED driver
-freeze(R + "/firmware/rope/lib", ("lora/__init__.py", "lora/modem.py",
-                                  "lora/sx126x.py", "lora/sync_modem.py",
-                                  "lora/async_modem.py"))
+# Radio driver stack (shared, lives under rope/lib) + OLED driver
+freeze(R + "/firmware/rope/lib", ("sx1262.py", "sx126x.py", "_sx126x.py"))
 freeze(R + "/firmware/winch", "ssd1306.py")
 
 # Shared modules
